eb922fbdb38aa5eedf965c47e959f1572dffe1e2
crnicavic <cavic5000@tutanota.com>
2025-11-14 16:06:15 +0100

    create structure of the project

---
+++ projekat/ConsistencyService/ConsistencyService/IService1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ConsistencyService
+{
+    [DataContract]
+    public class SensorInfo
+    {
+        [DataMember(Order = 1, IsRequired = true)]
+        public int publisher_id;
+        [DataMember(Order = 2, IsRequired = true)]
+        public float temperature;
+        [DataMember(Order = 3, IsRequired = true)]
+        public DateTime timestamp;
+    }
+
+    [ServiceContract]
+    public interface IPublisher
+    {
+        [OperationContract(IsOneWay = true)]
+        void publishTemp(SensorInfo t);
+    }
+
+    [ServiceContract]
+    public interface ISubscriber
+    {
+        [OperationContract(IsOneWay = true)]
+        SensorInfo querySensor(int id);
+    }
+
+}
+
+    
+
